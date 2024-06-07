57eef3ad5116b2ce6d68e89f46bc80eca1f4d7da
Gaurav Arbat <gauravrarbar@gmail.com>
2024-06-08 00:56:25 +0530

    first commit

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-slate-200'>
+      <div className='container mx-auto p-4'>
+        <p className='text-center font-bold' title="Youtube Channel">&copy; 2024 The Platform | Designed by <a href="https://gauravarbat.netlify.app" target='blank'>@GauravArbat</a></p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
